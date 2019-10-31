7444540c5a9e887f4519dd11bc512197a4f5bd75
Bryan.Marchena <bryanmarchena@gmail.com>
2019-10-31 04:33:48 -0400

    Add event listeners to buttons

--- start.js
+++ start.js
@@ -1,8 +1,11 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds){
+    clearInterval(countdown);
+
     const now = Date.now();
     const then = now + seconds * 1000;
     displayTimeLeft(seconds);
@@ -33,6 +36,13 @@ function displayEndTime(timestamp){
     const hour = end.getHours();
     const mins = end.getMinutes();
 
-    endTime.textContent = `Be Back at ${hour > 12 ? hour - 12 : hour}:${mins}`
+    endTime.textContent = `Be Back at ${hour > 12 ? hour - 12 : hour}:${mins < 10 ? '0': ''}${mins}`
     
 }
+
+function startTimer(){
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
