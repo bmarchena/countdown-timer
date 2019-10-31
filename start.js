ffdda8d873a59f4509d02edbcc3983f1b86b0414
Bryan.Marchena <bryanmarchena@gmail.com>
2019-10-31 04:28:43 -0400

    Fix remaining time display issue and display return time

--- start.js
+++ start.js
@@ -1,10 +1,12 @@
 let countdown;
-const timerDisplay = document.querySelector('.display__time-left')
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 function timer(seconds){
     const now = Date.now();
     const then = now + seconds * 1000;
     displayTimeLeft(seconds);
+    displayEndTime(then);
 
     countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -24,4 +26,13 @@ function displayTimeLeft(seconds){
     
     timerDisplay.textContent = display;
     console.log({mins, remSeconds});
+}
+
+function displayEndTime(timestamp){
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const mins = end.getMinutes();
+
+    endTime.textContent = `Be Back at ${hour > 12 ? hour - 12 : hour}:${mins}`
+    
 }
